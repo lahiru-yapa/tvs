4d8942e5107206263e1db83bd07cc2546c5177d4
lahiru-yapa <yapavidu@gmail.com>
2025-07-26 09:17:13 -0700

    1

--- resources/views/includes/js.blade.php
+++
@@ -1,5 +0,0 @@
-   <!--======== SCRIPT FILES =========-->
-   <script src="{{ asset('Public/js/jquery.min.js') }}"></script>
-    <script src="{{ asset('Public/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('Public/js/materialize.min.js') }}"></script>
-    <script src="{{ asset('Public/js/custom.js') }}"></script>
